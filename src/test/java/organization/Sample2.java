bad17f937e3b5c9613b7a23bf55980a8a73e8be0
Kavita128 <janakiias123@gmail.com>
2025-12-30 15:54:57 +0530

    hey

--- src/test/java/organization/Sample2.java
+++ src/test/java/organization/Sample2.java
@@ -2,7 +2,7 @@ package organization;
 
 public class Sample2 {
 public static void main(String[] args) {
-	System.out.println("heyyy");
+	System.out.println("It's so hectic to lear ut i still like it");
 }
 	// TODO Auto-generated method stub
 
