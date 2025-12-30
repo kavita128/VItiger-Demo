50bc25958aea73f279751ee520487c18066d849b
kavita128 <janakiias123@gmail.com>
2025-12-30 15:59:14 +0530

    Update print statement in SampleForGitHub.java

--- src/test/java/organization/SampleForGitHub.java
+++ src/test/java/organization/SampleForGitHub.java
@@ -7,6 +7,6 @@ public class SampleForGitHub
 		System.out.println("hi GitHub");
 		System.out.println("hi kavita, just now i update this");
 		System.out.println("hi hi");
-		
+		System.out.println("hi hi kavita");
 	}
 }
